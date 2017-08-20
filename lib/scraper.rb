d37da60b786d205b5a5a791ec1be0d12ba6cd584
jlmack13 <jennimacklin@gmail.com>
2017-08-20 12:41:25 -0400

    added the get_page and get_heroes methods to the scraper class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,17 @@ require 'pry'
 
 class Scraper
 
+  #get the page
+  def get_page
+    Nokogiri::HTML(open("https://playoverwatch.com/en-us/heroes/"))
+  end
+
+  #get the heroes
+  def get_heroes
+    self.get_page.css(".hero-portrait-detailed")
+  end
+
+  #make each hero
+
+  #print the list of heroes
 end
