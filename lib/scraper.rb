ea611df0db04f8f41a28c7bab8205ac37d6ef578
jlmack13 <jennimacklin@gmail.com>
2017-08-20 14:41:35 -0400

    working on hero details scraper still...

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,18 +22,15 @@ class Scraper
   def scrape_hero_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
-    hero = {
-      :overview => doc.css("#overview .hero-detail-description").text,
-      :quote => doc.css(".hero-detail.title").text
-    }
-    hero_abilities = {}
+    hero = []
+    hero_abilities = []
     doc.css(".hero-ability").each do |ability|
       hero_ability = ability.css(".hero-ability-descriptor .h5").text
       hero_description = ability.css(".hero-ability-descriptor p").text
       hero_abilities << {ability: hero_ability, description: hero_description}
     end
     hero << hero_abilities
-    hero_biography = {}
+    hero_biography = []
     doc.css("#story .hero-bio").each do |info|
       hero_real_name = info.css(".name .hero-bio-copy").text
       hero_occupation = info.css(".occupation .hero-bio-copy").text
@@ -41,9 +38,14 @@ class Scraper
       hero_affiliation = info.css(".affiliation .hero-bio.copy").text
       hero_biography << {real_name: hero_real_name, occupation: hero_occupation, base: hero_base, affiliation: hero_affiliation}
     end
+    hero_quote = doc.css(".hero-detail.title").text
+    hero_overview = doc.css("#overview .hero-detail-description").text
+    hero << hero_quote
+    hero << hero_overview
     hero << hero_biography
+    hero
   end
-
+  binding.pry
 
 
 
