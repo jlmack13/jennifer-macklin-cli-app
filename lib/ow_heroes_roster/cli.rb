fbbd787d89de817e85e70716d94a2aedb4004323
jlmack13 <jennimacklin@gmail.com>
2017-08-21 11:34:06 -0400

    added logic to cli class to get user input after displaying instructions. exiting the program works, others tbd

--- lib/ow_heroes_roster/cli.rb
+++ lib/ow_heroes_roster/cli.rb
@@ -12,16 +12,23 @@ class OwHeroesRoster::CLI
 
   #ask user if they'd like to select a hero (by name or number?) for more details, reprint the list, or exit the program
   def menu
-
     puts "\n---***------ INSTRUCTIONS ------***---"
-    puts "\n1. To see more information, enter the hero's name or number."
+    puts "\n1. To see details about a hero, enter the hero's name or number."
     puts "2. To see this list again, type 'list'."
     puts "3. To exit the program, type 'exit'."
+    input = gets.strip.downcase
+    if input == 'list'
+      list_heroes
+    elsif input == 'exit'
+      exit
+    else
+      display_hero(input)
+    end
   end
 
   #display the details of the requested hero. do I want this here?
-  def display_hero
-
+  def display_hero(input)
+    #validate input (here or above?)
   end
 
 end
