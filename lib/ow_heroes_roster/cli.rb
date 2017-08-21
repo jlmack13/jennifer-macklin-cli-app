c7137e9b7264f8904f9130adc8ab4bb4a54611ae
jlmack13 <jennimacklin@gmail.com>
2017-08-21 10:42:10 -0400

    stubbed out cli class methods

--- lib/ow_heroes_roster/cli.rb
+++ lib/ow_heroes_roster/cli.rb
@@ -4,4 +4,19 @@ class OwHeroesRoster::CLI
     puts "Welcome to the Overwatch Heroes Roster Gem!"
   end
 
+  #print the list of heroes scraped from the site
+  def list_heroes
+
+  end
+
+  #ask user if they'd like to select a hero (by name or number?) for more details, reprint the list, or exit the program
+  def menu
+
+  end
+
+  #display the details of the requested hero. do I want this here?
+  def display_hero
+
+  end
+
 end
