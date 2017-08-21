1f84c9552d51cefc2ebc4618000bb9cba90aa61f
jlmack13 <jennimacklin@gmail.com>
2017-08-21 11:21:19 -0400

    added instructions to the user to the menu method in the cli class

--- lib/ow_heroes_roster/cli.rb
+++ lib/ow_heroes_roster/cli.rb
@@ -2,6 +2,7 @@ class OwHeroesRoster::CLI
 
   def call
     puts "Welcome to the Overwatch Heroes Roster Gem!"
+    menu
   end
 
   #print the list of heroes scraped from the site
@@ -12,6 +13,10 @@ class OwHeroesRoster::CLI
   #ask user if they'd like to select a hero (by name or number?) for more details, reprint the list, or exit the program
   def menu
 
+    puts "\n---***------ INSTRUCTIONS ------***---"
+    puts "\n1. To see more information, enter the hero's name or number."
+    puts "2. To see this list again, type 'list'."
+    puts "3. To exit the program, type 'exit'."
   end
 
   #display the details of the requested hero. do I want this here?
